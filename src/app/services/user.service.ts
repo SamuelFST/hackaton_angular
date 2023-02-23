4cec67b2a345652d5931a8e281538c326f262c6a
SamuelFST <guti.samuel2002@gmail.com>
2023-02-23 15:41:24 -0300

    user service to communicate with the api

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+
+import { environment } from 'src/environments/environment';
+import User from '../Objects/User';
+import UserRegister from '../Objects/UserRegister';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private readonly API_URL = environment.API_URL;
+
+  constructor(private $http: HttpClient) { }
+
+  listAll = () => {
+    return this.$http.get<User[]>(`${this.API_URL}/users`);
+  }
+
+  findById = (id: number) => {
+    return this.$http.get<User>(`${this.API_URL}/users/${id}`);
+  }
+
+  searchByName = (name: string) => {
+    return this.$http.get<User[]>(`${this.API_URL}/users?name=${name}`);
+  }
+
+  create = (user: UserRegister) => {
+    return this.$http.post(`${this.API_URL}/users`, user);
+  }
+
+  update = (user: UserRegister, id: number) => {
+    return this.$http.put(`${this.API_URL}/users/${id}`, user);
+  }
+
+  delete = (id : number) => {
+    return this.$http.delete(`${this.API_URL}/users/${id}`);
+  }
+}
